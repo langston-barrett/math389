9a2654f2f1e17349bfc5105e645377281490a17e
Langston Barrett <langston.barrett@gmail.com>
2016-09-22 06:00:12 +0000

    minunit: include tested file name

--- minunit.h
+++ minunit.h
@@ -1,12 +1,15 @@
 /* file: langston-minunit.h */
+#include <string.h>
+
 #define assert(message, test) do { if (!(test)) return message; } while (0)
 
 #define run_test(test) do { char *message = test(); tests_run++; \
     if (message) return message; } while (0)
 extern int tests_run;
 
+#define __BASENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #define test_main(all_tests_function) do { char *result = all_tests_function(); \
-    if (result == 0) printf("All tests passed.\n"); \
-    else if (result != 0) printf("fail: %s\n", result); \
-    printf("Tests run: %d\n", tests_run); \
+    if (result == 0) printf("%s: All tests passed.\n", __BASENAME__); \
+    else if (result != 0) printf("%s: fail: %s\n", __BASENAME__, result); \
+    printf("%s: Tests run: %d\n", __BASENAME__, tests_run);              \
     return result != 0; } while (0)
