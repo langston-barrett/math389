78bb922bed9a05f15809798b6353a5efb261f0c8
Langston Barrett <langston.barrett@gmail.com>
2016-10-10 04:38:49 +0000

    csv: revise function signature

--- ps05/cache/csv.c
+++ ps05/cache/csv.c
@@ -5,10 +5,13 @@
    @brief Turn two arrays of ints into a valid CSV file!
    @param arr1 The first array
    @param arr2 The second array
-   @param size1 |arr1|
-   @param size2 |arr2|
+   @param size The lengths of the arrays (they must be the same length).
    @return The string representation of the CSV
 */
-char *csv(int64_t *arr1, int64_t *arr2, int64_t size1, int64_t size2) {
+char *csv(int64_t *arr1, int64_t *arr2, int64_t size) {
+  for (int64_t i = 0; i < size1)
   return "";
+}
+
+void str_to_file(char *str) {
 }
