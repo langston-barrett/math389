6ec1b1586e34e5fa82d25a6c33b1a3c5bab452fe
gatlinnewhouse <gatlinnewhouse@gmail.com>
2016-10-09 23:53:27 -0700

    fix some make errors`

--- ps05/cache/cache.c
+++ ps05/cache/cache.c
@@ -6,25 +6,25 @@ void main() {}
 
 double *find_cache_size(int stepSize, int64_t expectedCacheSize) {
   int64_t arraySize = expectedCacheSize / 8;
-  double *timeArray[arraySize];
+  double *timeArray = malloc(sizeof(double)*arraySize);
   srand(time(NULL));
-  for(int i = 0; i < arraySize; i = i + stepsize) {
+  for(int i = 0; i < arraySize; i = i + stepSize) {
     int64_t arrayOfTestValues[i];
     /* Seed the array with random values */
     for (int k = 0; k < i; k++) {
-      arrayOfTestValue[k] = (int64_t)rand();
+      arrayOfTestValues[k] = (int64_t)rand();
     }
     int64_t x = (int64_t)rand();
     /* Warm up the array */
     for (int l = 0; i < 10; l++) {
       for (int k = 0; k < i; k++) {
-        x = x ^ arrayOfTestValue[k];
+        x = x ^ arrayOfTestValues[k];
       }
     }
     /* Time the array cache fetching for this i */
     clock_t begin = clock();
     for (int k = 0; k < i; k++) {
-      x = x ^ arrayOfTestValue[k];
+      x = x ^ arrayOfTestValues[k];
     }
     clock_t end = clock();
     timeArray[i] = (double)(end - begin) / CLOCKS_PER_SEC;
