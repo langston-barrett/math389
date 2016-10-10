fb84eed2773df856df7e99ca129aa2d62a835e3c
gatlinnewhouse <gatlinnewhouse@gmail.com>
2016-10-09 21:35:43 -0700

    Finished test function for test 1 The function accepts a max cache value in terms of total bytes/64 right now. The array for testing and the array for the timer are set to the value of this variable. This needs to be changed to cache_size=m arr_size=(m/8)+1 and I will work on that next

--- ps05/cache/cache.c
+++ ps05/cache/cache.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <time.h>
 
 void main() {
 }
@@ -9,9 +10,18 @@ void testacache(int64 max_cache) {
   for (int step = 0; step < max_cache; step++) {
     arrayOfTestValues[step] = step;
   }
+  /* Warm up the cache by moving through the array once */
   int64 x = random_int64();
-  for (int i = 0; i < max_cache; i++) {
+  for (int i = 0; i < 10*(max_cache); i++) {
     x = x ^ arrayOfTestValues[i];
   }
-  
+  /* Time the cache by running through the array ten times */
+  x = random_int64();
+  double timerArray[max_cache];
+  for (int i =0; i < 10*(max_cache); i++) {
+    clock_t begin = clock();
+    x = x ^ arrayOfTestValues[i];
+    clock_t end = clock();
+    timerArray[i] = (double)(end - begin) / CLOCK_PER_SEC;
+  }
 }
