67f58015a82f97bd6b113161df96ed942f0f87a6
Langston Barrett <langston.barrett@gmail.com>
2016-10-24 00:04:32 +0000

    default.nix: improvements

--- ps05/cache/default.nix
+++ ps05/cache/default.nix
@@ -1,8 +1,13 @@
 # See README.md for explanation.
-with import <nixpkgs> {};
-  rWrapper.override {
-  packages = with rPackages; [
+# To use: nix-shell --run zsh
+# Optimally we'd use: nix-shell --pure --run zsh
+# https://nixos.org/wiki/Development_Environments
+# http://nixos.org/nix/manual/#sec-nix-shell
+with import <nixpkgs> {}; stdenv.mkDerivation {
+  name = "dots";
+  buildInputs = with rPackages; [
     knitr
     ggplot2
+    zsh
   ];
 }
