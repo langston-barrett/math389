aa356ab2ca1640c6e01e5e06dfe13ae302394b43
Langston Barrett <langston.barrett@gmail.com>
2016-10-27 22:33:11 -0700

    updates to size analysis

--- ps05/cache/main.c
+++ ps05/cache/main.c
@@ -6,6 +6,7 @@
 const int64_t l1_guess = 512000; // 64kb in bits
 const int64_t l2_guess = 4100000; // 512kb in bits
 const int64_t l3_guess = 128000000; // 16mb in bits
+const int64_t l3_guess2 = 256000000; // 32mb in bits
 
 // from http://www.cpu-world.com/CPUs/Bulldozer/AMD-FX-Series%20FX-8320.html
 // Cache: 	L1 data 	L1 instruction 	L2 	L3
@@ -13,10 +14,10 @@ const int64_t l3_guess = 128000000; // 16mb in bits
 int main(void) {
   printf("Attempting to find size of L1 cache...\n");
 
-  int64_t l1_step_size = 1;
-  int64_t length_times_L1 = l1_guess / (8 * l1_step_size);
+  int64_t l1_step_size = 100;
+  int64_t length_times_L1 = l3_guess2 / (8 * l1_step_size);
   int64_t *sizes = malloc(length_times_L1*sizeof(int64_t));
-  double *arr = find_cache_size(l1_step_size, l1_guess, 10);
+  double *arr = find_cache_size(l1_step_size, l3_guess2, 10);
 
   sizes[0] = 0;
   for (int64_t i = 1; i < length_times_L1; i++) {
