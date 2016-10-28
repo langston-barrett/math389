240f9f8cd475c577f6ff451e256433b279cd3fd3
gatlinnewhouse <gatlinnewhouse@gmail.com>
2016-10-28 15:21:38 -0700

    talked with Jim, we could work more this weekend @siddharthist

--- ps05/cache/cache-size.c
+++ ps05/cache/cache-size.c
@@ -18,7 +18,10 @@ double *find_cache_size(int step_size, int64_t expected_cache_size, int64_t trav
   // each entry is 64 bits = 8 bytes, step size is in entries
   int64_t arraySize = expected_cache_size / (4 * step_size);
   double *times = malloc(arraySize * sizeof(double));
-
+  //vary stride length (iterate through)
+  //to find the cache size look at time per element
+  // vary the array size and look at the 2nd run through after 1 warm up
+  // to find the cache penalty then just look at the # of times fetched and the timining
   int64_t x = (int64_t)rand();
   for (int i = 0; i < arraySize; i = i + step_size) {
     // printf("Examining array of length %i\n", i);
